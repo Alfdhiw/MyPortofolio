4fb7bfc5b1e8a66d720eeaf794a4ed1f21564b73
Alfdhiw <wibowalif22@gmail.com>
2023-05-22 08:47:48 +0700

    Update V.4

---
+++ dist/js/custom.js
@@ -0,0 +1,110 @@
+﻿
+/*=============================================================
+    Authour URI: www.binarytheme.com
+    License: Commons Attribution 3.0
+
+    http://creativecommons.org/licenses/by/3.0/
+
+    100% Free To use For Personal And Commercial Use.
+    IN EXCHANGE JUST GIVE US CREDITS AND TELL YOUR FRIENDS ABOUT US
+   
+    ========================================================  */
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+        scroll_fun: function () {
+
+            /*====================================
+             SCROLL SCRIPTS 
+            ======================================*/
+            $(function () {
+                $('.move-me a').bind('click', function (event) { //just pass move-me in design and start scrolling
+                    var $anchor = $(this);
+                    $('html, body').stop().animate({
+                        scrollTop: $($anchor.attr('href')).offset().top
+                    }, 1000, 'easeInOutQuad');
+                    event.preventDefault();
+                });
+            });
+
+            
+
+        },
+        
+        gallery_fun: function () {
+            /*====================================
+                 FOR IMAGE/GALLERY POPUP
+            ======================================*/
+            $("a.preview").prettyPhoto({
+                social_tools: false
+            });
+
+            /*====================================
+            FOR IMAGE/GALLERY FILTER
+            ======================================*/
+
+            // MixItUp plugin
+            // http://mixitup.io
+
+            $('#port-folio').mixitup({
+                targetSelector: '.portfolio-item',
+                filterSelector: '.filter',
+
+
+            });
+        },
+        vedio_fun:function()
+        {
+            $(function () {
+                $(".player").mb_YTPlayer();
+            });
+        },
+
+        nice_scroll_fun:function()
+        {
+            $("html").niceScroll();
+        },
+        custom_fun:function()
+        {
+            /*====================================
+             WRITE YOUR   SCRIPTS  BELOW
+            ======================================*/
+
+
+
+
+        },
+
+    }
+   
+   
+    $(document).ready(function () {
+        mainApp.scroll_fun();
+        mainApp.gallery_fun();
+        mainApp.vedio_fun();
+        mainApp.nice_scroll_fun();
+        mainApp.custom_fun();
+
+    });
+}(jQuery));
+
+// Smooth scrolling using jQuery easing
+$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 56)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+// Closes responsive menu when a scroll trigger link is clicked
+$('.js-scroll-trigger').click(function() {
+});
+
+
